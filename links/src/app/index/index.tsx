1d96e8c0e8b97fa2a5d7783d5c8185c54faea331
Thiago Mota <thiagomotape@hotmail.com.br>
2024-12-30 22:01:07 -0300

    criei o componente category e deixei-o dinamico e tipado com props e typescript, adicionei tambem as props do Pressable atravez da herança do POO

--- links/src/app/index/index.tsx
+++ links/src/app/index/index.tsx
@@ -2,6 +2,7 @@ import { View, Image, TouchableOpacity } from "react-native"
 import { styles } from "./styles"
 import { colors } from "@/styles/colors"
 import { MaterialIcons } from "@expo/vector-icons"
+import { Category } from "@/components/category"
 
 export default function Index() {
     return (
@@ -12,6 +13,10 @@ export default function Index() {
                     <MaterialIcons name="add" size={32} color={colors.green[300]} />
                 </TouchableOpacity>
             </View>
+
+            <Category name="Projeto" icon="code"/>
+            <Category name="Site" icon="language"/>
+            <Category name="Video" icon="movie"/>
         </View>
     )
 }
---
+++ links/src/components/category/styles.ts
@@ -0,0 +1,16 @@
+import { colors } from "@/styles/colors";
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container: {
+        flexDirection: "row",
+        alignItems: "center",
+        gap: 5,
+        padding: 20,
+    },
+    name: {
+        fontSize: 16,
+        fontWeight: "600",
+        color: colors.gray[400],
+    }
+})
