549725b35d9f2e128bb701c82eb8cda502dbe1a0
Thiago Mota <thiagomotape@hotmail.com.br>
2024-12-31 16:51:48 -0300

    criei o modal para visualizar os detalhes de cada link e os botões de excluir e abrir link

--- links/src/app/index/index.tsx
+++ links/src/app/index/index.tsx
@@ -1,9 +1,10 @@
-import { View, Image, TouchableOpacity, FlatList } from "react-native"
+import { View, Image, TouchableOpacity, FlatList, Modal, Text } from "react-native"
 import { styles } from "./styles"
 import { colors } from "@/styles/colors"
 import { MaterialIcons } from "@expo/vector-icons"
 import { Categories } from "@/components/categories"
 import { Link } from "@/components/link"
+import { Option } from "@/components/option"
 
 export default function Index() {
     return (
@@ -24,6 +25,24 @@ export default function Index() {
             contentContainerStyle={styles.linksContainer}
             showsVerticalScrollIndicator={false}
             />
+            <Modal transparent visible={true}>
+                <View style={styles.modal}>
+                    <View style={styles.modalContent}>
+                        <View style={styles.modalHeader}>
+                            <Text style={styles.modalCategory}>Site</Text>
+                            <TouchableOpacity>
+                               <MaterialIcons name="close" size={20} color={colors.gray[400]}/>
+                            </TouchableOpacity>
+                        </View>
+                        <Text style={styles.modaLinkName}>Portfolio</Text>
+                        <Text style={styles.modaUrl}>thiagomota.tech</Text>
+                        <View style={styles.modalFooter}>
+                            <Option name="Excluir" icon="delete" variant="secondary"/>
+                            <Option name="Abrir" icon="language" />
+                        </View>
+                    </View>
+                </View>
+            </Modal>
         </View>
     )
 }
--- links/src/app/index/styles.ts
+++ links/src/app/index/styles.ts
@@ -25,5 +25,46 @@ export const styles = StyleSheet.create({
         gap: 21,
         padding: 18,
         paddingBottom: 100,
-    }
+    },
+    modal: {
+        flex: 1,
+        justifyContent: "flex-end"
+    },
+    modalContent: {
+        backgroundColor: colors.gray[900],
+        borderTopWidth: 1,
+        borderTopColor: colors.gray[800],
+        paddingBottom: 42,
+        padding: 24,
+    },
+    modalHeader: {
+        width: "100%",
+        flexDirection: "row",
+        alignItems: "center",
+        marginBottom: 32,
+    },
+    modalCategory: {
+        flex: 1,
+        fontSize: 16,
+        fontWeight: "500",
+        color: colors.gray[400],
+    },
+    modaLinkName: {
+        fontSize: 18,
+        fontWeight: "600",
+        color: colors.gray[200],
+    },
+    modaUrl: {
+        fontSize: 14,
+        color: colors.gray[400],
+    },
+    modalFooter: {
+        flexDirection: "row",
+        marginTop: 32,
+        width: "100%",
+        justifyContent: "space-around",
+        borderTopWidth: 1,
+        borderTopColor: colors.gray[600],
+        paddingVertical: 14,
+    },
 })
