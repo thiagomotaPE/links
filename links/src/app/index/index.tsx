b139c5210cc394b0325bfdb2a993e1b913f592ae
Thiago Mota <thiagomotape@hotmail.com.br>
2025-01-01 22:36:55 -0300

    fazendop filtro por categoria da lista de links

--- links/src/app/index/index.tsx
+++ links/src/app/index/index.tsx
@@ -16,7 +16,8 @@ export default function Index() {
     async function getLinks() {
         try {
             const response = await linkStorage.get()
-            setLinks(response)
+            const filtered = response.filter((link) => link.category === category)
+            setLinks(filtered)
         } catch (error) {
             Alert.alert("Erro", "Não foi possivel salvar o link")
             console.log(error) 
